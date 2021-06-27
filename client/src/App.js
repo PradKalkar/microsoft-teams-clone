37a19d5bb0d22e74755004a0a890aed1d6137020
Pradnesh Kalkar <ppk.kalkar@gmail.com>
2021-06-27 20:27:56 +0530

    Fix build issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Auth from './routes/Auth';
 import CreateRoom from "./routes/CreateRoom";
 import Room from "./routes/Room";
 import './App.css'
@@ -9,7 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/auth" exact component={Auth} />
         <Route path="/" exact component={CreateRoom} />
         <Route path="/room/:roomID" component={Room} />
       </Switch>
