033dc4931f9f49fe18327aab8033af70a9350c63
Pradnesh Kalkar <52340022+PradKalkar@users.noreply.github.com>
2021-07-06 00:52:32 +0530

    Add env variables to secret

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = secrets.REACT_APP_AUTH0_DOMAIN;
+const clientId = secrets.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
